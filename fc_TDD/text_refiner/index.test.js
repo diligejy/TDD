d9a7b1fd0b7104c5abfcea46346c4b7159099050
diligejy <sjy049@gmail.com>
2022-06-19 15:47:21 +0900

    테스트 우선 개발

--- fc_TDD/text_refiner/index.test.js
+++ fc_TDD/text_refiner/index.test.js
@@ -1,3 +1,4 @@
+const faker = require('faker');
 // sut = system under test
 const sut = require("./index");
 
@@ -32,8 +33,39 @@ test.each`
     ${"hello  world"}   | ${"hello world"}
     ${"hello   world"}  | ${"hello world"}
     ${"hello    world"} | ${"hello world"}
+    ${"hello     world"} | ${"hello world"}
+    ${"hello      world"} | ${"hello world"}
+    ${"hello       world"} | ${"hello world"}
 `('sut transforms "$source" to "$expected"', ({ source, expected }) => {
     const actual = sut(source);
     expect(actual).toBe(expected);
 })
 
+test.each`
+    source | expected 
+    ${"hello\t world"} | ${"hello world"}
+    ${"hello \tworld"} | ${"hello world"}
+`('sut transforms "$source" that contains tab character to "$expected"', ({ source, expected }) => {
+    const actual = sut(source);
+    expect(actual).toBe(expected);
+})
+
+test.each`
+    source | bannedWords | expected 
+    ${"hello mockist"} | ${["mockist", "purist"]} |${"hello *******"}  
+    ${"hello purist"} | ${["mockist", "purist"]} |${"hello ******"}  
+`('sut transforms "$source" that contains bannedWords to "$expected"', ({ source, bannedWords, expected }) => {
+    const actual = sut(source, { bannedWords });
+    expect(actual).toBe(expected);
+})
+
+describe('given banned word', () => {
+    const bannedWord = faker.lorem.word();
+    const source = "hello " + bannedWord;
+    const expected = "hello " + "*".repeat(bannedWord.length);
+
+    test(`${bannedWord} when invoke sut then it returns ${expected}`, () => {
+        const actual = sut(source, { bannedWords: [bannedWord] })
+        expect(actual).toBe(expected)
+    })
+})
